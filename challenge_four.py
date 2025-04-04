b14433978f82a8d04275f6c063a0a5c453137582
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-04-04 10:04:00 -0500

    Create challenge_four.py

---
+++ challenge_four.py
@@ -0,0 +1,17 @@
+number = input("Enter a number: ")
+
+int initialList = []
+int i = 1
+
+for i < number:
+  initialList += 1
+
+for j in range(0, len(initialList))
+  if j%5 == 0 && j%3 == 0
+    initialList[j] = "FizzBuzz"
+  elif j%5 == 0 && j%3 != 0
+    initialList[j] = "Buzz"
+  elif j%5 != 0 && j%3 == 0
+    initialList[j] = "Fizz"
+  
+print ("Results: [initialList]")
