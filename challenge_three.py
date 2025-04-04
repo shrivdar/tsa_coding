83c770ec5494ba0cf9bac95c2e4ad0562010c6a2
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-04-04 10:04:11 -0500

    Create challenge_three.py

---
+++ challenge_three.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def Main():
+  int total = 0
+  int average = 0
+  dataset = input("Please enter the dataset: ")
+  my_dict = {}
+  for i in range(0, len(string), 3):
+    total += int(string[i])
+    my_dict[string[i]] += 1
+
+  average = total/i
+  print("Average of the dataset: " + average)
+
+  for value in mydict.values():
+    print(my_dict[value] + " appeared " + value + " times within the dataset")
