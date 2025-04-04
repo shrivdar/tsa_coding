a087846c1bd979256a2224a6aa9c4e9e7bb3a108
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-04-04 09:10:42 -0500

    Create challenge_one.py

---
+++ challenge_one.py
@@ -0,0 +1,70 @@
+def main():
+
+  print("In order to convert the provided temperature please input the temperature and first initial of the scale (F- Fahrenheit, C - Celsius, K - Kelvin)")
+  int K;
+  int C;
+  int F;
+  String unit;
+  int converted_temperature;
+
+  
+  try
+  def kelvinToCelsius():
+    K = C + 273.15
+  def celciusToKelvin():
+    C = K - 273.15
+  def kelvinToFahrenheit():
+    F = (K-273.15)(9/5) + 32
+  def FahrenheitToKelvin():
+    K = (F-32)(5/9) + 273.15
+  def FahrenheitToCelsius():
+    C = (F-32)(5/9)
+  def CelciusToFahrenheit():
+    F = C*(9/5) + 32
+    
+
+  
+  print("In order to convert the provided temperature please input the temperature and first initial of the scale (F- Fahrenheit, C - Celsius, K - Kelvin)")
+  degree = input("Please enter temperature and scale: ")
+  scale = degree[-1]
+  temperature = append[degree]
+  print("Conversion Options: #1. Convert to Celsius #2. Convert to Fahrenheit #3. Convert to Kelvin")
+  statement = input("Please select the scale you wish to convert your temperature to: ")
+  conversionUnit = int(statement)
+  
+  if unit == "C":
+    print(
+      elif statement == "1":
+        print("Temperature is already in the desired scale")
+        
+      elif statement == "2":
+        CelciusToFahrenheit(
+
+      elif statement == "3":
+        celciusToKelvin(
+          
+    if unit == "F":
+    print(
+      elif statement == "1":
+        FahrenheitToCelsius(
+        
+      elif statement == "2":
+        print("Temperature is already in the desired scale")
+
+      elif statement == "3":
+        FahrenheitToKelvin(
+
+    if unit == "C":
+    print(
+      elif statement == "1":
+        kelvinToCelsius(
+        
+      elif statement == "2":
+        kelvinToFahrenheit(
+
+      elif statement == "3":
+        print("Temperature is already in the desired scale")
+  print("Converted Temperature: [converted_temperature]")
+
+
+  
