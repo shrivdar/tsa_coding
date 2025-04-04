dc011468f3bfbe75b40d6c984e270017fd5f9a48
Darsh Shrivastava <160412468+shrivdar@users.noreply.github.com>
2025-04-04 18:08:41 -0500

    Update challenge_one.py

--- challenge_one.py
+++ challenge_one.py
@@ -9,41 +9,40 @@ def main():
 
   
   try
-  def kelvinToCelsius():
-    K = C + 273.15
-  def celciusToKelvin():
-    C = K - 273.15
-  def kelvinToFahrenheit():
-    F = (K-273.15)(9/5) + 32
-  def FahrenheitToKelvin():
-    K = (F-32)(5/9) + 273.15
-  def FahrenheitToCelsius():
-    C = (F-32)(5/9)
-  def CelciusToFahrenheit():
-    F = C*(9/5) + 32
+  def kelvinToCelsius(temperature):
+    K = temperature + 273.15
+  def celciusToKelvin(temperature):
+    C = temperature - 273.15
+  def kelvinToFahrenheit(temperature):
+    F = (temperature-273.15)(9/5) + 32
+  def FahrenheitToKelvin(temperature):
+    K = (temperature-32)(5/9) + 273.15
+  def FahrenheitToCelsius(temperature):
+    C = (temperature-32)(5/9)
+  def CelciusToFahrenheit(temperature):
+    F = temperature*(9/5) + 32
     
 
   
   print("In order to convert the provided temperature please input the temperature and first initial of the scale (F- Fahrenheit, C - Celsius, K - Kelvin)")
   degree = input("Please enter temperature and scale: ")
   scale = degree[-1]
-  temperature = append[degree]
+  temperature = int(degree[:-1])
   print("Conversion Options: #1. Convert to Celsius #2. Convert to Fahrenheit #3. Convert to Kelvin")
   statement = input("Please select the scale you wish to convert your temperature to: ")
-  conversionUnit = int(statement)
   
-  if unit == "C":
+  if degree == "C":
     print(
       elif statement == "1":
         print("Temperature is already in the desired scale")
         
       elif statement == "2":
-        CelciusToFahrenheit(
+        CelciusToFahrenheit(temperature)
 
       elif statement == "3":
         celciusToKelvin(
           
-    if unit == "F":
+    if degree == "F":
     print(
       elif statement == "1":
         FahrenheitToCelsius(
@@ -54,7 +53,7 @@ def main():
       elif statement == "3":
         FahrenheitToKelvin(
 
-    if unit == "C":
+    if degree == "C":
     print(
       elif statement == "1":
         kelvinToCelsius(
